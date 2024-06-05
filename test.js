145092142e8366f560ada2a511e15e4a8756bcc8
cimifahmi21 <fahmiirvansyah21@gmail.com>
2024-06-05 18:14:54 +0700

    sudah beres halaman test

---
+++ test.js
@@ -0,0 +1,16 @@
+let age = 21
+
+if (age >= 17) {
+    console.log('you can now create an Id Card')
+} else {
+    console.log('You Are not old enogh to create an Id Card')
+}
+
+let kondisi = "Senang"
+
+if (kondisi == "Pusing") {
+    console.log('Belajar Terus lah')
+} else {
+    console.log ('Lanjut main')
+}
+
